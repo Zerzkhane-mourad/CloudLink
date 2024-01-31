ecf5bc0b248a0f27d37db149790449fe3755be4e
zerzkhane <zerzkhanemourad@gmail.com>
2024-01-31 12:47:51 +0100

    add env

--- client/utils/db.ts
+++ client/utils/db.ts
@@ -4,14 +4,13 @@ let client: MongoClient;
 
 export const connectToDatabase = async (): Promise<Db> => {
   if (!client) {
-    const uri = "mongodb://127.0.0.1:27017/";
+    const uri = process.env.MONGODB_URI ;
 
     if (!uri) {
       throw new Error('MONGODB_URI is not defined in the environment variables.');
     }
 
     client = await MongoClient.connect(uri);
-
   }
 
   return client.db();
