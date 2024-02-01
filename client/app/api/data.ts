a520a948eb58d6fb1e7fbd910932ee1179dde0c0
zerzkhane <zerzkhanemourad@gmail.com>
2024-02-01 01:32:13 +0100

    replace folder api

---
+++ client/app/api/data.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { connectToDatabase } from '@/utils/db';
+
+export default async (_req: NextApiRequest, res: NextApiResponse) => {
+  
+  try {
+    const db = await connectToDatabase();
+    const collection = db.collection('users'); 
+    const data = await collection.find({}).toArray();
+
+    res.status(200).json({ data });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
